bcd855787577ec58dde8f342ef8faa6c11c31a26
Dieferson Weren <dieferson.weren@hotmail.com>
2021-03-01 20:16:40 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Maria");
 		
-		System.out.println(obj);
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(1, "Maria", "maria@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
